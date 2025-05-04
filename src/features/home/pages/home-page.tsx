ebfd074754dc0a54a3752acfe3d397e3ed9d7873
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-05-04 00:33:25 -0300

    feat: add home page component

---
+++ src/features/home/pages/home-page.tsx
@@ -0,0 +1,8 @@
+export function HomePage() {
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center">
+      <h1 className="text-8xl font-semibold">Home Page</h1>
+      <p className="text-5xl">Welcome to the home page!</p>
+    </main>
+  )
+}
