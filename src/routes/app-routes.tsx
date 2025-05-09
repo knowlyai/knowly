c6d81f5e02cb259729e697eef09aeed7d51ee107
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-05-08 23:13:16 -0300

    refactor: remove useless imports

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -1,20 +1,15 @@
 import { HomePage } from '@/features/home/pages/home-page'
 import { Page } from '@/shared/components/page'
-import { Suspense, lazy } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 export function AppRoutes() {
   return (
     <Suspense>
       <Routes>
-        {/* <Route path="/login" element={<LoginPage />} /> */}
-
         <Route element={<Page />}>
           <Route path="/" element={<HomePage />} />
         </Route>
-
-        {/* <Route path="/404" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate to="/404" replace />} /> */}
       </Routes>
     </Suspense>
   )
