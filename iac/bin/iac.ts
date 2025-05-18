869e16c5682101e959ad254e8e6d1a80ae066abb
Rubiozito <rafaelrubiocarnes@gmail.com>
2025-05-18 19:42:22 -0300

    fix: fix stack name

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -18,7 +18,7 @@ const tags = {
   owner: 'Knowly'
 }
 
-new IacStack(app, 'IacStack', {
+new IacStack(app, stackName, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
