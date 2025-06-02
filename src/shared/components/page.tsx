ca05652a0218d1bad253cadd27159eba1ef7a090
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-06-02 18:46:06 -0300

    refactor: change toast position

--- src/shared/components/page.tsx
+++ src/shared/components/page.tsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast'
 export function Page() {
   return (
     <>
-      <Toaster position="top-right" reverseOrder={false} />
+      <Toaster position="bottom-right" reverseOrder={false} />
       <Navbar />
       <Outlet />
     </>
