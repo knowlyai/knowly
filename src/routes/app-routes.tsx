5065375e6d0578d028e200940b544633ef56b2ae
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2025-06-08 19:42:24 -0300

    feat: add route to page

--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -3,6 +3,7 @@ import { ForgotPasswordPage } from '@/features/forgot-password/pages/forgot-pass
 import { ResetPasswordPage } from '@/features/reset-password/pages/reset-password'
 import { LoginPage } from '@/features/login/pages/login'
 import { SignUpPage } from '@/features/sign-up/pages/sign-up-page'
+import { SubscriptionManagementPage } from '@/features/user-area/pages/subscription-management'
 import { Page } from '@/shared/components/page'
 import { Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
@@ -17,6 +18,10 @@ export function AppRoutes() {
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/forgot-password" element={<ForgotPasswordPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route
+            path="/subscription-management"
+            element={<SubscriptionManagementPage />}
+          />
           <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
