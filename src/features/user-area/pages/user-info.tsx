08ff7aebe9b72f7b8580e83b530856660fdff94d
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-07-27 15:00:16 -0300

    feat: refactor UserInfoPage layout and add Background component

--- src/features/user-area/pages/user-info.tsx
+++ src/features/user-area/pages/user-info.tsx
@@ -28,6 +28,7 @@ import {
   formatCPF,
   formatPhone
 } from '@/shared/utils/format-documents'
+import { Background } from '@/shared/components/background'
 
 // Usuário simulado (como se estivesse logado)
 const mockUser: UserInfoData & { id: string } = {
@@ -114,201 +115,117 @@ export function UserInfoPage() {
   }
 
   return (
-    <Layout className="bg-background min-h-screen min-w-screen">
-      <main className="mt-20 flex flex-1 flex-col items-center justify-center p-12">
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          className="w-full max-w-xl"
-        >
-          <section>
-            <h1 className="text-foreground mb-6 text-center text-2xl font-semibold drop-shadow-xl sm:text-5xl">
-              Dados de cadastro
-            </h1>
-            {!isEditing ? (
-              <div className="space-y-4">
-                <div>
-                  <Label>Nome</Label>
-                  <Input value={user.name} disabled />
-                </div>
-                <div>
-                  <Label>E-mail</Label>
-                  <Input value={user.email} disabled />
-                </div>
-                <div>
-                  <Label>Telefone/Celular</Label>
-                  <Input value={user.phone} disabled />
-                </div>
-                <div>
-                  <Label>Tipo de Pessoa</Label>
-                  <Input
-                    value={
-                      user.documentType === DOCUMENT_TYPE.INDIVIDUAL
-                        ? 'Pessoa física'
-                        : 'Pessoa jurídica'
-                    }
-                    disabled
-                  />
-                </div>
-                <div>
-                  <Label>Documento</Label>
-                  <Input
-                    value={
-                      user.documentType === DOCUMENT_TYPE.INDIVIDUAL
-                        ? formatCPF(user.document)
-                        : formatCNPJ(user.document)
-                    }
-                    disabled
-                  />
-                </div>
-                <div>
-                  <Label>Data de Nascimento</Label>
-                  <Input
-                    value={user.birthDate?.toLocaleDateString('pt-BR')}
-                    disabled
-                  />
-                </div>
-                <Button
-                  type="button"
-                  className="mt-6 flex w-full items-center justify-center gap-2"
-                  onClick={handleEdit}
-                >
-                  <Pencil size={20} /> Editar perfil
-                </Button>
-              </div>
-            ) : (
-              <TooltipProvider>
-                <Form {...form}>
-                  <form
-                    className="space-y-4"
-                    onSubmit={form.handleSubmit(onSubmit)}
+    <Background className="min-h-full">
+      <Layout className="h-full w-full">
+        <main className="flex flex-col items-center justify-center p-12">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="w-full max-w-xl"
+          >
+            <section>
+              <h1 className="text-foreground mb-6 text-center text-2xl font-semibold drop-shadow-xl sm:text-5xl">
+                Dados de cadastro
+              </h1>
+              {!isEditing ? (
+                <div className="space-y-4">
+                  <div>
+                    <Label>Nome</Label>
+                    <Input value={user.name} disabled />
+                  </div>
+                  <div>
+                    <Label>E-mail</Label>
+                    <Input value={user.email} disabled />
+                  </div>
+                  <div>
+                    <Label>Telefone/Celular</Label>
+                    <Input value={user.phone} disabled />
+                  </div>
+                  <div>
+                    <Label>Tipo de Pessoa</Label>
+                    <Input
+                      value={
+                        user.documentType === DOCUMENT_TYPE.INDIVIDUAL
+                          ? 'Pessoa física'
+                          : 'Pessoa jurídica'
+                      }
+                      disabled
+                    />
+                  </div>
+                  <div>
+                    <Label>Documento</Label>
+                    <Input
+                      value={
+                        user.documentType === DOCUMENT_TYPE.INDIVIDUAL
+                          ? formatCPF(user.document)
+                          : formatCNPJ(user.document)
+                      }
+                      disabled
+                    />
+                  </div>
+                  <div>
+                    <Label>Data de Nascimento</Label>
+                    <Input
+                      value={user.birthDate?.toLocaleDateString('pt-BR')}
+                      disabled
+                    />
+                  </div>
+                  <Button
+                    type="button"
+                    className="mt-6 flex w-full items-center justify-center gap-2"
+                    onClick={handleEdit}
                   >
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Nome</FormLabel>
-                          <FormControl>
-                            <Input {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>E-mail</FormLabel>
-                          <FormControl>
-                            <Input {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="phone"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Telefone/Celular</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              onChange={(e) =>
-                                handlePhoneChange(
-                                  e.target.value,
-                                  field.onChange
-                                )
-                              }
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="documentType"
-                      render={({ field: _ }) => (
-                        <FormItem>
-                          <FormLabel>Tipo de Pessoa</FormLabel>
-                          <FormControl>
-                            <Tooltip>
-                              <TooltipTrigger asChild>
-                                <Input
-                                  value={
-                                    user.documentType ===
-                                    DOCUMENT_TYPE.INDIVIDUAL
-                                      ? 'Pessoa física'
-                                      : 'Pessoa jurídica'
-                                  }
-                                  disabled
-                                />
-                              </TooltipTrigger>
-                              <TooltipContent>
-                                <p>Não é possível alterar o tipo de pessoa</p>
-                              </TooltipContent>
-                            </Tooltip>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    <FormField
-                      control={form.control}
-                      name="document"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Documento</FormLabel>
-                          <FormControl>
-                            <Input
-                              {...field}
-                              onChange={(e) => {
-                                if (
-                                  form.getValues('documentType') ===
-                                  'individual'
-                                ) {
-                                  handleCPFChange(
-                                    e.target.value,
-                                    field.onChange
-                                  )
-                                } else {
-                                  handleCNPJChange(
-                                    e.target.value,
-                                    field.onChange
-                                  )
-                                }
-                              }}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                    {user.documentType === DOCUMENT_TYPE.INDIVIDUAL && (
+                    <Pencil size={20} /> Editar perfil
+                  </Button>
+                </div>
+              ) : (
+                <TooltipProvider>
+                  <Form {...form}>
+                    <form
+                      className="space-y-4"
+                      onSubmit={form.handleSubmit(onSubmit)}
+                    >
                       <FormField
                         control={form.control}
-                        name="birthDate"
+                        name="name"
                         render={({ field }) => (
                           <FormItem>
-                            <FormLabel>Data de Nascimento</FormLabel>
+                            <FormLabel>Nome</FormLabel>
+                            <FormControl>
+                              <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>E-mail</FormLabel>
+                            <FormControl>
+                              <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Telefone/Celular</FormLabel>
                             <FormControl>
                               <Input
-                                type="date"
-                                value={
-                                  field.value
-                                    ? new Date(field.value)
-                                        .toISOString()
-                                        .split('T')[0]
-                                    : ''
-                                }
+                                {...field}
                                 onChange={(e) =>
-                                  field.onChange(new Date(e.target.value))
+                                  handlePhoneChange(
+                                    e.target.value,
+                                    field.onChange
+                                  )
                                 }
                               />
                             </FormControl>
@@ -316,30 +233,116 @@ export function UserInfoPage() {
                           </FormItem>
                         )}
                       />
-                    )}
-                    <div className="mt-6 flex gap-4">
-                      <Button
-                        type="button"
-                        variant="secondary"
-                        className="w-1/2"
-                        onClick={handleCancel}
-                      >
-                        Cancelar
-                      </Button>
-                      <Button
-                        type="submit"
-                        className="flex w-1/2 items-center justify-center gap-2"
-                      >
-                        <Save size={20} /> Salvar
-                      </Button>
-                    </div>
-                  </form>
-                </Form>
-              </TooltipProvider>
-            )}
-          </section>
-        </motion.div>
-      </main>
-    </Layout>
+                      <FormField
+                        control={form.control}
+                        name="documentType"
+                        render={({ field: _ }) => (
+                          <FormItem>
+                            <FormLabel>Tipo de Pessoa</FormLabel>
+                            <FormControl>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <Input
+                                    value={
+                                      user.documentType ===
+                                      DOCUMENT_TYPE.INDIVIDUAL
+                                        ? 'Pessoa física'
+                                        : 'Pessoa jurídica'
+                                    }
+                                    disabled
+                                  />
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                  <p>Não é possível alterar o tipo de pessoa</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="document"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Documento</FormLabel>
+                            <FormControl>
+                              <Input
+                                {...field}
+                                onChange={(e) => {
+                                  if (
+                                    form.getValues('documentType') ===
+                                    'individual'
+                                  ) {
+                                    handleCPFChange(
+                                      e.target.value,
+                                      field.onChange
+                                    )
+                                  } else {
+                                    handleCNPJChange(
+                                      e.target.value,
+                                      field.onChange
+                                    )
+                                  }
+                                }}
+                              />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      {user.documentType === DOCUMENT_TYPE.INDIVIDUAL && (
+                        <FormField
+                          control={form.control}
+                          name="birthDate"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>Data de Nascimento</FormLabel>
+                              <FormControl>
+                                <Input
+                                  type="date"
+                                  value={
+                                    field.value
+                                      ? new Date(field.value)
+                                          .toISOString()
+                                          .split('T')[0]
+                                      : ''
+                                  }
+                                  onChange={(e) =>
+                                    field.onChange(new Date(e.target.value))
+                                  }
+                                />
+                              </FormControl>
+                              <FormMessage />
+                            </FormItem>
+                          )}
+                        />
+                      )}
+                      <div className="mt-6 flex gap-4">
+                        <Button
+                          type="button"
+                          variant="secondary"
+                          className="w-1/2"
+                          onClick={handleCancel}
+                        >
+                          Cancelar
+                        </Button>
+                        <Button
+                          type="submit"
+                          className="flex w-1/2 items-center justify-center gap-2"
+                        >
+                          <Save size={20} /> Salvar
+                        </Button>
+                      </div>
+                    </form>
+                  </Form>
+                </TooltipProvider>
+              )}
+            </section>
+          </motion.div>
+        </main>
+      </Layout>
+    </Background>
   )
 }
