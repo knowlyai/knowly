fcf7a7f002d898dbeaec418da24b1e006ff9df56
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-08-07 20:12:58 -0300

    feat: add delete file and get knowledge base query hooks to knowledge base service

--- src/features/user-area/hooks/use-kb.ts
+++ src/features/user-area/hooks/use-kb.ts
@@ -1,13 +1,31 @@
 import {
   CreateKnowledgeBaseRequest,
+  DeleteFileRequest,
   knowledgeBaseService
 } from '@/services/knowledge-base'
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 
 export const useCreateKnowledgeBaseMutation = () => {
   return useMutation({
     mutationFn: async (data: CreateKnowledgeBaseRequest) => {
       return await knowledgeBaseService.createKnowledgeBase(data)
+    }
+  })
+}
+
+export const useDeleteFileMutation = () => {
+  return useMutation({
+    mutationFn: async (data: DeleteFileRequest) => {
+      return await knowledgeBaseService.deleteFile(data)
+    }
+  })
+}
+
+export const useGetKnowledgeBaseQuery = (kbId?: string) => {
+  return useQuery({
+    queryKey: ['knowledgeBases', kbId],
+    queryFn: async () => {
+      return await knowledgeBaseService.getKnowledgeBase({ kbId })
     }
   })
 }
