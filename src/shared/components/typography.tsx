4a665f4dd7d4641c175399d05f148a257a1b95c4
Júlia Galhardi <juliagalhardi.c@gmail.com>
2025-08-16 17:34:45 -0300

    style: adjust padding top

--- src/shared/components/typography.tsx
+++ src/shared/components/typography.tsx
@@ -10,7 +10,7 @@ export function h1({ children }: { children: React.ReactNode }) {
 
 export function h2({ children }: { children: React.ReactNode }) {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2 className="scroll-m-20 border-b pt-6 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
       {children}
     </h2>
   )
