8b986dae3afb324e25d896ae973771550a60a451
Enzo Sakamoto <enzo.sak@hotmail.com>
2025-08-24 12:34:57 -0300

    feat: add getAuthHeader function to retrieve authorization header with access token

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -8,3 +8,10 @@ export const api = axios.create({
     Accept: 'application/json'
   }
 })
+
+export const getAuthHeader = () => {
+  const token = localStorage.getItem('access_token') || ''
+  return {
+    Authorization: `Bearer ${token}`
+  }
+}
