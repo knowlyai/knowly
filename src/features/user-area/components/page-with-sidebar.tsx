e6abb3e707a13a323d97c1f1f98a20bc67a9d1d1
Izabel Sampaio Goes <izabel.sampaio120@gmail.com>
2025-09-15 12:25:31 -0300

    fix: padding left

--- src/features/user-area/components/page-with-sidebar.tsx
+++ src/features/user-area/components/page-with-sidebar.tsx
@@ -1,81 +1,17 @@
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Navbar } from '@/shared/components/navbar'
 import { Toaster } from 'react-hot-toast'
-import { SidebarProvider } from '@/components/ui/sidebar'
-import {
-  MainSidebar,
-  SidebarItem,
-  SidebarSections
-} from '@/shared/components/sidebar'
-import { User, BrainCircuit, Wallet, LogOut } from 'lucide-react'
+import { Sidebar } from '@/shared/components/user-area-sidebar'
 
 export function UserAreaPageWithSidebar() {
-  const navigate = useNavigate()
-
-  const items_user: SidebarItem[] = [
-    { label: 'Dados de cadastro', icon: <User />, key: 'user' },
-    { label: 'Minhas bases', icon: <BrainCircuit />, key: 'bases' },
-    { label: 'Assinatura', icon: <Wallet />, key: 'subscription' }
-  ]
-
-  const sections: SidebarSections[] = [
-    {
-      label: 'Área do Usuário',
-      items: items_user
-    }
-  ]
-
-  function handleLogout() {
-    // Adicionar lógica de desfazer o login (deslogar o usuário)
-    navigate('/')
-  }
-
-  const logout: SidebarItem = {
-    label: 'Sair',
-    icon: <LogOut />,
-    key: '/',
-    variant: 'red_centered',
-    onClick: handleLogout
-  }
-
   return (
     <>
       <Toaster position="bottom-right" reverseOrder={false} />
       <Navbar />
-      <div className="mt-18 flex min-h-screen w-full">
-        <SidebarProvider>
-          <MainSidebar
-            sections={sections}
-            footer={logout}
-            variant="sidebar"
-            showTrigger={false}
-          />
-          <main>
-            <div className="items-center justify-center">
-              <Outlet />
-            </div>
-          </main>
-        </SidebarProvider>
+      <div className="flex h-screen w-full pt-18">
+        <Sidebar />
+        <Outlet />
       </div>
     </>
   )
 }
-
-
-// import { Outlet } from 'react-router-dom'
-// import { Navbar } from '@/shared/components/navbar'
-// import { Toaster } from 'react-hot-toast'
-// import { Sidebar } from '@/shared/components/sidebar'
-
-// export function PageWithSidebar() {
-//   return (
-//     <>
-//       <Toaster position="bottom-right" reverseOrder={false} />
-//       <Navbar />
-//       <div className="flex h-screen w-full pt-18">
-//         <Sidebar />
-//         <Outlet />
-//       </div>
-//     </>
-//   )
-// }
